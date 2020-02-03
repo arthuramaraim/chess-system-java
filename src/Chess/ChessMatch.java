5742dedf30f42c029e4fa6959493a0e8e664a70b
Arthur Amaraim <akamaraim@inf.ufsm.br>
2020-02-03 13:21:57 -0300

    contador de movimentos de peça

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -121,6 +121,7 @@ public class ChessMatch {
         }
     }
 
+
     private void validateSourcePosition(Position position){
     if (!board.thereIsAPiece(position)){
         throw  new ChessException("Não existe peça na possivel de origem");
